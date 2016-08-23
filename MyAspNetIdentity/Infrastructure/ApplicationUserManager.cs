2cd74ddc2eee56f56aa02539d7180b61bf43a895
Jonny <jonathan.beresford86@gmail.com>
2016-08-23 17:27:10 +0100

    Time to run it

--- MyAspNetIdentity/Infrastructure/ApplicationUserManager.cs
+++ MyAspNetIdentity/Infrastructure/ApplicationUserManager.cs
@@ -1,7 +1,24 @@
-﻿namespace MyAspNetIdentity.Infrastructure
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace MyAspNetIdentity.Infrastructure
 {
-    public class ApplicationUserManager
+    public class ApplicationUserManager : UserManager<ApplicationUser>
     {
-        
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+
+        //todo: this is where we pass in the context and get the user manager back
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            var appDbContext = context.Get<ApplicationDbContext>();
+            var appUserManager = new ApplicationUserManager(new UserStore<ApplicationUser>(appDbContext));
+
+            return appUserManager;
+        }
     }
 }
